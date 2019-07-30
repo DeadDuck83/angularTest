bfa8b70c4adc40325f1462335df70b0795eaf04f
Derek Moore <moore8577@gmail.com>
2019-07-30 08:55:37 -0700

    switching to angular 8 tutorial

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -5,18 +5,11 @@ import { Component } from "@angular/core";
   selector: "courses", //<courses>
   template: `
     <h2>{{ title }}</h2>
-    <ul>
-      <li *ngFor="let course of courses">
-        {{ course }}
-      </li>
-    </ul>
+    <h2 [textContent]="title">dfdf</h2>
+    <img src="{{ imageUrl }} " alt="" />
   `
 })
 export class CoursesComponent {
   title = "List of courses";
-  courses;
-
-  constructor(service: CoursesService) {
-    this.courses = service.getCourses();
-  }
+  imageUrl = "http://lorempixel.com/400/200";
 }
